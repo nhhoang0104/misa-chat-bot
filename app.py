5cac2456eac0396d8086380e34eb103b1430a485
nhhoang0104 <nhhoang010420@gmail.com>
2025-08-22 10:30:55 +0700

    update

---
+++ app.py
@@ -0,0 +1,165 @@
+import streamlit as st
+from datetime import datetime, timedelta
+import pytz
+
+st.set_page_config(layout="wide", page_title="Agent Chat", page_icon="🤖")
+
+import time
+import asyncio
+import json
+from uuid import uuid4
+from typing import AsyncGenerator
+
+def get_current_time() -> str:
+    now = datetime.now(pytz.timezone("Asia/Ho_Chi_Minh"))
+    if now.weekday() < 6:
+        weekday = f"thứ {now.weekday() + 2}"
+    else:
+        weekday = "chủ nhật"
+
+    current_time = f"{weekday}, ngày {now.day:02d}/{now.month:02d}/{now.year}"
+    return current_time
+
+def get_this_week_time() -> dict:
+    now = datetime.now(pytz.timezone("Asia/Ho_Chi_Minh"))
+
+    monday = now - timedelta(now.weekday() - 0)
+    monday = f"ngày {monday.day:02d}/{monday.month:02d}/{monday.year}"
+
+    sunday = now + timedelta(6 - now.weekday())
+    sunday = f"ngày {sunday.day:02d}/{sunday.month:02d}/{sunday.year}"
+    return {
+        "monday": monday,
+        "sunday": sunday
+    }
+
+# ========== Page Config ==========
+st.title("Chat with Agent")
+st.button("Clear message", on_click=lambda: st.session_state.clear())
+
+# ========== Init Session State ==========
+if "session_id" not in st.session_state:
+    st.session_state.session_id = str(uuid4())
+
+if "messages" not in st.session_state:
+    st.session_state.messages = []
+
+if "config" not in st.session_state:
+    current_time = get_current_time()
+    week_time = get_this_week_time()
+
+    st.session_state.config = {
+        "configurable": {
+            "env": "test",
+            "app": "Agent",
+            "user_id": "Guest",
+            "user_name": "Guest",
+            "gender": "Male",
+            "x_birthdate": "01/01/2000",
+            "response_markdown": True,
+            "message_id": "",
+            "version": 4,
+            "language": "",
+            "this_monday": week_time["monday"],
+            "this_sunday": week_time["sunday"],
+            "current_time": current_time,
+            "thread_id": st.session_state.session_id,
+        },
+        "recursion_limit": 15
+    }
+
+# ========== Config UI ==========
+with st.expander("Config"):
+    config = st.session_state.config["configurable"]
+    col1, col2, col3 = st.columns(3)
+    config["user_name"] = col1.text_input("User Name", value=config["user_name"])
+    config["gender"] = col2.text_input("Gender", value=config["gender"])
+    config["x_birthdate"] = col3.text_input("Date of Birth", value=config["x_birthdate"])
+
+    if st.button("Submit Config"):
+        session_id = str(uuid4())
+        st.session_state.session_id = session_id
+
+        st.session_state.config = {
+            "configurable": config
+        }
+
+
+# ========== Process Events ==========
+async def process_events(inputs: dict) -> AsyncGenerator[str, None]:
+    pass
+    # async for event in graph.astream_events(inputs, config=st.session_state.config, version="v1"):
+    #     kind = event["event"]
+    #
+    #     if kind == "on_chat_model_stream":
+    #         content = event["data"]["chunk"].content
+    #         if content:
+    #             yield content
+    #
+    #     elif kind == "on_tool_start":
+    #         tool_name = event["name"]
+    #         tool_input = event["data"].get("input", {})
+    #         output = f"\n\n➡️ Tool `{tool_name}` called\n\n"
+    #
+    #         if tool_input:  # Only add content if input is not empty
+    #             # Format tool input consistently
+    #             if isinstance(tool_input, str):
+    #                 formatted_input = tool_input
+    #             else:
+    #                 try:
+    #                     formatted_input = json.dumps(tool_input, indent=2, ensure_ascii=False, default=str)
+    #                 except:
+    #                     formatted_input = str(tool_input)
+    #
+    #             output += f" with:\n```\n{formatted_input}\n```"
+    #
+    #         yield output
+    #
+    #     elif kind == "on_tool_end":
+    #         pass
+
+
+# ========== Async to Sync Generator ==========
+def to_sync_generator(async_func, *args, **kwargs):
+    async_gen = async_func(*args, **kwargs)
+    loop = asyncio.new_event_loop()
+    asyncio.set_event_loop(loop)
+    try:
+        while True:
+            try:
+                yield loop.run_until_complete(anext(async_gen))
+            except StopAsyncIteration:
+                break
+    finally:
+        loop.close()
+
+
+# ========== Show Chat History ==========
+for message in st.session_state.messages:
+    with st.chat_message(message["role"]):
+        if isinstance(message["content"], dict):
+            st.json(message["content"])
+        else:
+            st.markdown(message["content"], unsafe_allow_html=True)
+
+# ========== Chat Input ==========
+if prompt := st.chat_input("What is up?", max_chars=1000):
+    inputs = {"messages": [("user", prompt)]}
+
+    st.session_state.messages.append({"role": "user", "content": prompt})
+    with st.chat_message("user"):
+        st.markdown(prompt)
+
+    with st.chat_message("assistant"):
+        start_time = time.time()
+        response = st.write_stream(to_sync_generator(process_events, inputs))
+        end_time = time.time() - start_time
+
+        response_id = uuid4().hex
+        st.write(f"⏱️ **Processed in**: {round(end_time, 2)}s")
+        st.session_state.messages.append({
+            "role": "assistant",
+            "content": response,
+            "id": response_id,
+            "stars": 0
+        })
